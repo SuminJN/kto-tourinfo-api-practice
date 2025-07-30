0690db9753b7ab1146445362136ebef8bf22e0c5
Sumin Jeon <wjstnals1211@gmail.com>
2025-07-30 16:29:58 +0900

    초기 Gradle 프로젝트 설정 및 Spring Boot 기본 설정 파일 추가

---
+++ src/main/java/suminjn/test0724/TourApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package suminjn.test0724;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourApiPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourApiPracticeApplication.class, args);
+    }
+
+}
